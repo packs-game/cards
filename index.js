5c29dcda86efbc9833512d130e4dbe1d9544d615
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-24 23:02:59 -0500

    crud

--- index.js
+++ index.js
@@ -27,8 +27,36 @@ app.get('/cards', function(req, res) {
 	});
 });
 
-app.post('/cards', checkSuperAdmin, function(req,res){
-	res.send('NEW CARD ADDED')
+app.post('/card', checkSuperAdmin, function(req,res){
+	if (!req.body.card) { return res.status(400).send('No card submitted'); }
+	var c = new CardModel(req.body.card);
+	c.save(function(err, card){
+		return res.status(200).json(card);
+	});
+
+});
+
+app.post('/card/:id', checkSuperAdmin, function(req,res){
+	if (!req.body.card) { return res.status(400).send('No card submitted'); }
+	CardModel.findOne({_id: req.params.id }, function(err,card){
+		if (err || !card) { res.status(400).send('No card with id');}
+		for (var prop in req.body.card) {
+			card[prop] = req.body.card[prop];
+		}
+		card.save(function(err, card){
+			return res.status(200).json(card);
+		});
+	});
+});
+
+app.delete('/card/:id', checkSuperAdmin, function(req,res){
+	if (!req.params.id) { return res.status(400).send('No card submitted'); }
+	console.log(req.params.id);
+	CardModel.remove({_id: req.params.id }, function(err,card){
+		console.log(err,card)
+		if (err) { return res.status(400).send('No card with id');}
+		return res.sendStatus(200);
+	});
 });
 
 app.listen(3007);
